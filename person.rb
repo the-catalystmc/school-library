ca8d3ff2bbdd0daa5f998158969fa4787d982677
Rhyine <rhyinestewart@gmail.com>
2021-10-06 08:07:19 -0500

    Update person with an instance of Corrector

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
+require './corrector'
+
 class Person
   def initialize(id, age, name: 'unknown', parent_permission: true)
     @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
 
   attr_reader :id
@@ -16,6 +19,10 @@ class Person
   end
 
   public
+
+  def validate_name(name)
+    @corrector.correct_name(name)
+  end
 
   def can_use_services?
     of_age? || @parent_permission ? true : false
