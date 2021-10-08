9a958b5c3230e6c98c41bc5591d4d553a6a94ca0
Rhyine <rhyinestewart@gmail.com>
2021-10-08 10:58:57 -0500

    Update id to generate random number

--- person.rb
+++ person.rb
@@ -2,8 +2,8 @@ require './corrector'
 require './rental'
 
 class Person
-  def initialize(id, age, name = 'unknown', parent_permission: true)
-    @id = id
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
