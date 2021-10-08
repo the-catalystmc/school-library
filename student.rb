38d86a0142415617fe8749e87fa29183bb8a4635
Rhyine <rhyinestewart@gmail.com>
2021-10-08 18:24:01 -0500

    Fix instance variables

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, _name, _parent_permission, **args)
-    super(age, name, parent_permission: true, **args)
+  def initialize(classroom: nil, **args)
+    super(**args)
     @classroom = classroom.add_student(self) unless classroom.nil?
   end
 
